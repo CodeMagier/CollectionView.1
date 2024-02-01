fab37af249fcab6dbea15d460f5badb8e127c79e
CodeMagier <Ios23.git@gmail.com>
2024-02-01 11:12:11 +0600

    Initial Commit

---
+++ CollectionView.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView.1
+//
+//  Created by 1 on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
